786256bac7d4627e7692e54c04d42de73a94f11e
bramtechs <bramtech@telenet.be>
2023-03-20 18:13:52 +0100

    version 2.0

--- source/app.d
+++ source/app.d
@@ -5,15 +5,35 @@ import std.file;
 import std.utf;
 import std.string;
 import std.conv;
+import std.format;
 
 const string EXAMPLE_CMD = "Example: markdef2csv -i myMarkdownFile.md -o output.csv -f false -p $";
 
-struct Definition {
+const string[] MD_LIST_SYMBOLS = ["-", "+", "* "]; // <-- NOTE: the space after star
+// This is needed for situations like this: ``` * **Hello List Item** ```.
+
+struct Definition
+{
     string term;
     string explanation;
+
+    string toString() {
+        return format("\n%s\n%s", term, explanation);
+    }
 }
 
-string formatIfNeeded(string text, bool keep){
+string format_if_needed(string text, bool keep)
+{
+    // NOTE: remove list symbols
+    // TODO: Add support for numbered list
+    foreach (symb; MD_LIST_SYMBOLS){
+        string formText = text.chompPrefix(symb);
+        if (formText != text) {
+            text = formText.strip;
+            break;
+        }
+    }
+    
     if (!keep){
         text = text.replace("*","")
                    .replace("_","")
@@ -23,7 +43,8 @@ string formatIfNeeded(string text, bool keep){
     return text;
 }
 
-string defs_to_csv(Definition[] defs, string prefix = ""){
+string defs_to_csv(Definition[] defs, string prefix = "")
+{
     string csv;
     foreach (def; defs) {
         csv ~= prefix ~ def.term ~ "; " ~ def.explanation ~ "\n";
@@ -31,21 +52,8 @@ string defs_to_csv(Definition[] defs, string prefix = ""){
     return csv;
 }
 
-unittest {
-    Definition[] sample = [
-        {"This is a term","This is the explanation."},
-        {"This is another term","This is more explanation!"},
-    ];
-
-    string expected = "This is a term; This is the explanation.\n";
-    expected ~= "This is another term; This is more explanation!";
-
-    string result = defs_to_csv(sample);
-    writeln(result);
-    //assert(result == expected); // TODO FIX
-}
-
-Definition[] parse_markdown(string[] lines, bool keepFormat = false){
+Definition[] parse_markdown(string[] lines, bool keepFormat = false)
+{
     bool isBusy = false;
     Definition[] defs;
 
@@ -58,18 +66,21 @@ Definition[] parse_markdown(string[] lines, bool keepFormat = false){
 
     Definition nextDef;
     for (int i = 1; i < lines.length; i++){
-        // check if ': ' is first char
-        if (lines[i].startsWith(": ")) {
+        // NOTE: Trim the line first before checking
+        lines[i] = lines[i].strip;
+
+        // check if ':' is first char
+        if (lines[i].startsWith(":")) {
             if (isBusy){
-                string text = lines[i][2..$];
-                text = formatIfNeeded(text,keepFormat);
+                string text = lines[i][1..$].strip;
+                text = format_if_needed(text,keepFormat);
                 nextDef.explanation ~= " " ~ text.strip;
             } else {
                 isBusy = true;
-                nextDef.explanation = formatIfNeeded(lines[i][2..$],keepFormat);
+                nextDef.explanation = format_if_needed(lines[i][1..$].strip,keepFormat);
 
                 // look at previous line for term
-                nextDef.term = formatIfNeeded(lines[i-1].strip,keepFormat);
+                nextDef.term = format_if_needed(lines[i-1].strip,keepFormat);
             }
         }else if (isBusy){
             isBusy = false;
@@ -78,37 +89,6 @@ Definition[] parse_markdown(string[] lines, bool keepFormat = false){
     }
 
     return defs;
-}
-
-unittest {
-    string[] sample = [
-        "This is a term",
-        ": This is the explanation.",
-        "",                         
-        "This is another term      ",
-        ": This is the explanation.",
-        ": across multiple lines!",
-        " ",
-        "  This is yet another term      ",
-        ": _with formatted text_.",
-        ": **that can be kept or cleared**"
-    ];
-
-    Definition[] expected = [
-        {"This is a term","This is the explanation."},
-        {"This is another term","This is the explanation. across multiple lines!"},
-        {"This is yet another term","with formatted text. that can be kept or cleared"}
-    ];
-
-    Definition[] result = parse_markdown(sample, false);
-    writeln("result: " ~ result.to!string );
-    assert(result == expected);
-
-    result = parse_markdown(sample, true);
-    expected[2].term = "This is yet another term";
-    expected[2].explanation = "_with formatted text_. **that can be kept or cleared**";
-    writeln("result 2: " ~ result.to!string );
-    assert(result == expected);
 }
 
 void main(string[] args)
@@ -165,9 +145,151 @@ void main(string[] args)
         try {
             alias write = std.file.write;
             write(output, csv);
+            writeln("Wrote table into " ~ output);
         } catch (FileException e){
             writeln("Could write results to file " ~ output ~ "!");
             return;
         }
     }
 }
+
+// ===================================================
+//                      UNIT TESTS
+// ===================================================
+// defs_to_csv
+unittest {
+    Definition[] sample = [
+        {"This is a term","This is the explanation."},
+        {"This is another term","This is more explanation!"},
+    ];
+
+    string expected = "This is a term; This is the explanation.\n";
+    expected ~= "This is another term; This is more explanation!\n";
+
+    string result = defs_to_csv(sample);
+    assert(result == expected, "\n'%s'\ndoes not equal\n'%s'".format(result, expected)); 
+}
+
+// Oneliner
+unittest {
+    string[] sample = [
+         "This is a term",
+         ": This is the explanation."
+    ];
+
+    Definition expected = {
+        "This is a term",
+        "This is the explanation."
+    };
+
+    Definition[] result = parse_markdown(sample, false);
+    assert(result.length == 1);
+    assert(result[0] == expected, "'%s'\ndoes not equal to\n'%s'".format(result[0], expected));
+}
+
+// Multiple lines
+unittest {
+    string[] sample = [
+        "This is another term      ",
+        ": This is the explanation.",
+        ": across multiple lines!",
+    ];
+
+    Definition expected = {
+        "This is another term",
+        "This is the explanation. across multiple lines!"
+    };
+
+    Definition[] result = parse_markdown(sample, false);
+    assert(result.length == 1);
+    assert(result[0] == expected, "'%s'\ndoes not equal to\n'%s'".format(result[0], expected));
+}
+
+// Remove any markdown styling
+unittest {
+    string[] sample = [
+        "  This is yet another term      ",
+        ": _with formatted text_.",
+        ": **that can be kept or cleared**",
+    ];
+
+    Definition expected = {
+        "This is yet another term",
+        "with formatted text. that can be kept or cleared",
+    };
+
+    Definition[] result = parse_markdown(sample, false);
+    assert(result.length == 1);
+    assert(result[0] == expected, "'%s'\ndoes not equal to\n'%s'".format(result[0], expected));
+}
+
+// Keep any markdown styling
+unittest {
+    string[] sample = [
+        "  This is yet another term      ",
+        ": _with formatted text_.",
+        ": **that can be kept or cleared**",
+    ];
+
+    Definition expected = {
+        "This is yet another term",
+        "_with formatted text_. **that can be kept or cleared**",
+    };
+
+    Definition[] result = parse_markdown(sample, true);
+    assert(result.length == 1);
+    assert(result[0] == expected, "'%s'\ndoes not equal to\n'%s'".format(result[0], expected));
+}
+
+// Misaligned
+unittest {
+    string[] sample = [
+        "     - This is another term but indented and part of a list!",
+        "      : It's explanation is also indented.",
+        "    :But it's misaligned!",
+    ];
+
+    Definition expected = {
+        "This is another term but indented and part of a list!",
+        "It's explanation is also indented. But it's misaligned!"
+    };
+
+    Definition[] result = parse_markdown(sample, false);
+    assert(result.length == 1);
+    assert(result[0] == expected, "'%s'\ndoes not equal to\n'%s'".format(result[0], expected));
+}
+
+// Multiple test
+unittest {
+    string[] sample = [
+        "This is a term",
+        ": This is the explanation.",
+        "",                         
+        "This is another term      ",
+        ": This is the explanation.",
+        ": across multiple lines!",
+        " ",
+        "  This is yet another term      ",
+        ": _with formatted text_.",
+        ": **that can be kept or cleared**",
+    ];
+
+    Definition[] expected = [
+        {"This is a term","This is the explanation."},
+        {"This is another term","This is the explanation. across multiple lines!"},
+        {"This is yet another term","with formatted text. that can be kept or cleared"},
+    ];
+
+    Definition[] result = parse_markdown(sample, false);
+
+    assert(expected.length == result.length);
+    for (int i = 0; i < result.length; i++) {
+        assert(result[i] == expected[i], "'%s'\ndoes not equal to\n'%s'".format(result[i], expected[i]));
+    }
+
+    result = parse_markdown(sample, true);
+    expected[2].term = "This is yet another term";
+    expected[2].explanation = "_with formatted text_. **that can be kept or cleared**";
+    assert(result == expected);
+}
+
